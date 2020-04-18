4699370c41ba4f404af631069b2ffc23b28f4c1c
DeathWofl <saul021002@gmail.com>
2020-04-18 09:53:06 -0400

    Respuestas listas

--- route/respuesta.go
+++ route/respuesta.go
@@ -8,11 +8,75 @@ import (
 	"github.com/labstack/echo"
 )
 
-//GetRespuesta retorna especificamente una respuesta
+//GetRespuesta retorna especificamente una respuesta por su ID
 func GetRespuesta(c echo.Context) error {
-	DB := db.Init()
-	id := c.Param("id")
+	DB := db.DBManager()
+	id := c.Param("ID")
 	Resp := models.Respuesta{}
 	DB.Find(&Resp, id)
 	return c.JSON(http.StatusOK, Resp)
+}
+
+//Respuestas retorna todas las respuestas existentes
+func Respuestas(c echo.Context) error {
+	DB := db.DBManager()
+	Resp := []models.Respuesta{}
+	DB.Find(&Resp)
+	return c.JSON(http.StatusOK, Resp)
+}
+
+//PutRespuesta actualizar una respuesta
+func PutRespuesta(c echo.Context) error {
+
+	//abro conexion
+	DB := db.DBManager()
+
+	// tomo el ID de parametro
+	ID := c.Param("ID")
+
+	// Respuesta antes de actualizar
+	Resp := models.Respuesta{}
+	DB.Find(&Resp, ID)
+
+	//Datos de los campos a actualizar
+	PutUser := models.Respuesta{}
+	if err := c.Bind(PutUser); err != nil {
+		panic(err)
+	}
+
+	//Actualizando Datos
+	DB.Model(&Resp).Updates(PutUser)
+
+	return c.JSON(http.StatusOK, PutUser)
+}
+
+//PostRespuesta Agregar respuesta
+func PostRespuesta(c echo.Context) error {
+	DB := db.DBManager()
+
+	Resp := models.Respuesta{}
+	err := c.Bind(Resp)
+	if err != nil {
+		panic(err)
+	}
+
+	DB.Create(&Resp)
+	return c.JSON(http.StatusOK, Resp)
+}
+
+//BuscarRespuestas busca respuestas que correspondan a una pregunta
+func BuscarRespuestas(c echo.Context) error {
+	DB := db.DBManager()
+
+	Pregunt := models.Pregunta{}
+	c.Bind(Pregunt)
+
+	Respues := []models.Respuesta{}
+
+	err := DB.Model(&Respues).Related(&Pregunt)
+	if err != nil {
+		panic(err)
+	}
+
+	return c.JSON(http.StatusOK, Respues)
 }

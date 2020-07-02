9cb3e1a679449fbb0834f4e82014e0c5f9225e32
Jose <josea.dominique01@gmail.com>
2020-07-02 18:34:13 -0400

    model equipos

--- models/equipo.go
+++ models/equipo.go
@@ -9,6 +9,7 @@ type Equipo struct {
 	MatriculaE2 string `json:"MatriculaE2" gorm:"not null;type:varchar(15);"`
 	MatriculaE3 string `json:"MatriculaE3" gorm:"not null;type:varchar(15);"`
 	CodigoGrupo string `json:"CodigoGrupo" gorm:"not null;type:varchar(100);" sql:"index"`
+	ContraGrupo string `json:"ContraGrupo" gorm:"not null;type:varchar(100);"`
 	RegResps    []RegResp
 	RegTiempos  []RegTiempo
 	Token       string `gorm:"-"`

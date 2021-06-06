c150a762b1b944ef9b84bc93ff5b478118a7ff5b
Saul Pinales <saul021002@gmail.com>
2021-06-05 23:51:02 -0400

    <feat>: logger

--- go.mod
+++ go.mod
@@ -14,11 +14,16 @@ require (
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/swaggo/echo-swagger v1.1.0 // indirect
 	github.com/swaggo/swag v1.7.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
 	golang.org/x/tools v0.1.0 // indirect
 	gopkg.in/gormigrate.v1 v1.6.0 // indirect
 	gorm.io/driver/mysql v1.0.4
 	gorm.io/gorm v1.20.12
+	honnef.co/go/tools v0.0.1-2019.2.3 // indirect
 )
---
+++ internal/platform/logger/logger.go
@@ -0,0 +1,60 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger interface {
+	Info(message string, fields ...zap.Field)
+	Warn(message string, fields ...zap.Field)
+	Error(message string, fields ...zap.Field)
+	Fatal(message string, fields ...zap.Field)
+	Debug(message string, fields ...zap.Field)
+}
+
+// LogHandler implements Logger
+type LogHandler struct {
+	log *zap.Logger
+	// appName    string
+	// appVersion string
+}
+
+func (lh *LogHandler) Info(message string, fields ...zap.Field) {
+	lh.log.Info(message, fields...)
+}
+
+func (lh *LogHandler) Warn(message string, fields ...zap.Field) {
+	lh.log.Warn(message, fields...)
+}
+
+func (lh *LogHandler) Fatal(message string, fields ...zap.Field) {
+	lh.log.Fatal(message, fields...)
+}
+
+func (lh *LogHandler) Debug(message string, fields ...zap.Field) {
+	lh.log.Debug(message, fields...)
+}
+
+func (lh *LogHandler) Error(message string, fields ...zap.Field) {
+	lh.log.Error(message, fields...)
+}
+
+// New returns a new instance of LogHandler
+func New(skipStack uint) *LogHandler {
+	config := zap.NewProductionConfig()
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.StacktraceKey = ""
+	config.EncoderConfig = encoderConfig
+
+	logger, err := config.Build(zap.AddCallerSkip(int(skipStack)))
+	if err != nil {
+		panic(err)
+	}
+
+	return &LogHandler{
+		log: logger,
+	}
+}
